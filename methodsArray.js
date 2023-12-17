f9fdcf2d8353d121fe72e9555f79ef8d88c7204e
Patanineh <patanineh@gmail.com>
2023-12-18 03:11:20 +0700

    up

---
+++ methodsArray.js
@@ -0,0 +1,13 @@
+let arr1 = ["A", true, 2];
+
+console.log("\n*****P****\n");
+console.log(arr1.push("new"));
+console.log(arr1);
+console.log(arr1.pop());
+console.log(arr1);
+
+console.log("\n*****s****\n");
+console.log(arr1.unshift("new"));
+console.log(arr1);
+console.log(arr1.shift());
+console.log(arr1);
