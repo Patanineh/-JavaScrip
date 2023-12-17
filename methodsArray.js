02d916dae371bdef3e11525163356d8834a82c11
Patanineh <patanineh@gmail.com>
2023-12-18 04:16:25 +0700

    up

--- methodsArray.js
+++ methodsArray.js
@@ -11,3 +11,10 @@ console.log(arr1.unshift("new"));
 console.log(arr1);
 console.log(arr1.shift());
 console.log(arr1);
+
+console.log("\n*****c****\n");
+let arr2 = ["B", false, 3];
+let newArr = arr1.concat(arr2);
+let newArr2 = arr2.concat([1,2,3]);
+console.log(newArr);
+console.log(newArr2);
