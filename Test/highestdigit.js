1599aaeca1b3624ab381b4fb0271f98c71b07dda
Patanineh <patanineh@gmail.com>
2024-01-15 08:45:07 +0700

    1

---
+++ Test/highestdigit.js
@@ -0,0 +1,7 @@
+function highestdigit(){
+    
+}
+
+console.log(highestdigit(379));
+console.log(highestdigit(2));
+console.log(highestdigit(377401));
---
+++ Test/numbersplit.js
@@ -0,0 +1,9 @@
+function numbersplit(num) {
+    return [Math.floor(num / 2), Math.ceil(num /2)];
+}
+
+console.log(numbersplit(4));
+console.log(numbersplit(10));
+console.log(numbersplit(11));
+console.log(numbersplit(-9));
+console.log(numbersplit(-4));
